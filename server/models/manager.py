44e5bef966202a1d83e47fa5460ccd5c7fb9e623
tolamson2811 <tolamson2811@gmail.com>
2024-04-22 10:58:37 +0700

    feat: hotel api

--- server/models/manager.py
+++ server/models/manager.py
@@ -7,13 +7,12 @@ class Manager(Base):
 
     id = Column(Uuid, primary_key=True, index=True)
     user_id = Column(Uuid, ForeignKey('user.id'), nullable=False)
-    hotel_id = Column(Uuid, ForeignKey('hotel.id'), nullable=True)
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
     phone_num = Column(String, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
     updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
 
-    hotel = relationship("Hotel", back_populates="manager", uselist=False)
+    hotel = relationship("Hotel", back_populates="manager")
     user = relationship("User", back_populates="manager")
     employee = relationship("Employee", back_populates="manager")
---
+++ server/routes/hotelRouter.py
@@ -0,0 +1,31 @@
+from fastapi import Depends, HTTPException, APIRouter
+from sqlalchemy.orm import Session
+from controllers.hotelController import HotelController
+from database import get_db
+from schemas.hotelSchema import HotelSchema
+
+router = APIRouter(prefix="/hotel", tags=["Hotel"], responses={404: {"description": "Not found"}})
+
+@router.get("/")
+async def get_all_hotels(page: int = 1, size: int = 10, db: Session = Depends(get_db) ):
+    return HotelController.getAllHotels(page, size, db)
+
+@router.get("/popular")
+async def get_popular_hotel(db: Session = Depends(get_db)):
+    return HotelController.getPopularHotel(db)
+
+@router.get("/{id}")
+async def get_hotel_by_id(id: str, db: Session = Depends(get_db)):
+    return HotelController.getHotelById(id, db)
+
+@router.post("/")
+async def create_hotel(hotel: HotelSchema, db: Session = Depends(get_db)):
+    return HotelController.createHotel(hotel, db)
+
+@router.put("/{id}")
+async def update_hotel(id: str, hotel: HotelSchema, db: Session = Depends(get_db)):
+    return HotelController.updateHotel(id, hotel, db)
+
+@router.delete("/{id}")
+async def delete_hotel(id: str, db: Session = Depends(get_db)):
+    return HotelController.deleteHotel(id, db)
